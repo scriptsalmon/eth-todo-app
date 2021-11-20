33beff98ca36961aad93aca804861d3ab2a307fe
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-11-19 17:04:42 -0800

    index.html todo app structure built out with bootstrap

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -1,8 +1,25 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
-  //state variables written to blockchain
+  //taskCount is a state variable written to blockchain
   //public allows us to read taskCount
   uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+  //uint will be the key id of Task that we store in tasks
+  mapping(uint => Task) public tasks;
+
+  constructor() public {
+    createTask("Check your wallet");
+  }
+
+  function createTask(string memory _content) public {
+    taskCount ++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
 }
 

5628b0b5f11b5a11b806890cd9b28ef10ba879e6
scriptsalmon@gmail.com <scriptsalmon@gmail.com>
2021-11-20 16:50:03 -0800

    changed localhost to 8545

--- truffle-config.js
+++ truffle-config.js
@@ -2,7 +2,7 @@ module.exports = {
   networks: { // allows us to specify network, port 7545 connects to ganash 'RPC Server'
     development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545,
       network_id: "*" // Match any network id
     }
   },
